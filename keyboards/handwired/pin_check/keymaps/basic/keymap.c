5d8db21294af4fad4c49c25d631fdcd88f5c2f0c
Brian <brian@mutualaid.info>
2021-02-05 15:50:54 -0500

    pretty keymap

--- keyboards/handwired/pin_check/keymaps/basic/keymap.c
+++ keyboards/handwired/pin_check/keymaps/basic/keymap.c
@@ -17,7 +17,10 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE
+    _BASE,
+    _FUNCTION,
+    _LOWER,
+    _RAISE
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -26,13 +29,94 @@ enum layer_names {
 //    QMKURL
 //};
 
+#define FN MO(_FUNCTION)
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* QWERTY
+ * ,-------------------------------------------------------------------------------------------------------.
+ * |   ~   |   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |   0   |   -   |   =   |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Tab   |   Q   |   W   |   E   |   R   |   T   |   Y   |   U   |   I   |   O   |   P   |   [   |   ]   |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Ctrl  |   A   |   S   |   D   |   F   |   G   |   H   |   J   |   K   |   L   |   ;   |   '   | Enter |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Shift |   Z   |   X   |   C   |   V   |   B   |   N   |   M   |   ,   |   .   |   /   | Shift | Vol+  |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Fn    | Ctrl  |  Opt  |  Cmd  | Lower | Bkspc | Space | Raise | Home  |  End  | PgUp  | PgDn  | Vol-  |
+ * `-------------------------------------------------------------------------------------------------------'
+ */
     [_BASE] = LAYOUT_ortho_5x13(
-        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS,
-        KC_GRV, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,
-        MT(MOD_LCTL, KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_VOLU,
-        KC_NO, MOD_LCTL, KC_LOPT, KC_LCMD, KC_NO, KC_BSPC, KC_SPC, KC_NO, KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_VOLD
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_VOLU,
+        FN,      KC_LCTL, KC_LOPT, KC_LCMD, LOWER,   KC_BSPC, KC_SPC,  RAISE,   KC_HOME, KC_END,  KC_PGUP, KC_PGDN, KC_VOLD
+    ),
+
+/* Function
+ * ,-------------------------------------------------------------------------------------------------------.
+ * |  Esc  |  F1   |  F2   |  F3   |  F4   |  F5   |  F6   |  F7   |  F8   |  F9   |  F10  |  F11  |  F12  |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Tab   |  Rst  | Debug | EERst |       |       |       |       |   -   |   7   |   8   |   9   |   +   |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Ctrl  |       |       |       |       |       |       |       |   *   |   4   |   5   |   6   | Enter |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Shift |       |       |       |       |       |       |       |   /   |   1   |   2   |   3   |  Br+  |
+ * |-------------------------------------------------------------------------------------------------------|
+ * |       | Ctrl  |  Opt  |  Cmd  |       |  Del  | Space |       |       |   0   |       |       |  Br-  |
+ * `-------------------------------------------------------------------------------------------------------'
+ */
+    [_FUNCTION] = LAYOUT_ortho_5x13(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_TAB,  RESET,   DEBUG,   EEP_RST, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PMNS, KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_LCTL, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PENT,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PSLS, KC_P1,   KC_P2,   KC_P3,   KC_BRMU,
+        KC_NO,   KC_LCTL, KC_LOPT, KC_LCMD, KC_NO,   KC_DEL,  KC_SPC,  KC_NO,   KC_NO,   KC_P0,   KC_NO,   KC_NO,   KC_BRMD
+    ),
+
+/* Lower
+ * ,-------------------------------------------------------------------------------------------------------.
+ * |  Esc  |       |       |       |       |       |       |       |       |       |       |       |       |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Tab   |       |       |       |       |       |       |       |       |       |       |       |   \   |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Ctrl  |       |       |       |       |       |       |       |       |       |       |       | Enter |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Shift |       |       |       |       |       |       |       |       |       |       |       | Vol+  |
+ * |-------------------------------------------------------------------------------------------------------|
+ * |       | Ctrl  |  Opt  |  Cmd  |       |  Del  | Space |       |       |       |       |       | Vol-  |
+ * `-------------------------------------------------------------------------------------------------------'
+ */
+    [_LOWER] = LAYOUT_ortho_5x13(
+        KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_TAB,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSLS,
+        KC_LCTL, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PENT,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_VOLU,
+        KC_NO,   KC_LCTL, KC_LOPT, KC_LCMD, KC_NO,   KC_DEL,  KC_SPC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_VOLD
+    ),
+
+/* Raise
+ * ,-------------------------------------------------------------------------------------------------------.
+ * |       |       |       |       |       |       |       |       |       |       |       |       |       |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Tab   |       |       |       |       |       |       |       |       |       |       |       |   \   |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Ctrl  |       |       |       |       |       | Left  | Down  |  Up   | Right |       |       | Enter |
+ * |-------------------------------------------------------------------------------------------------------|
+ * | Shift |       |       |       |       |       |       |       |       |       |       |       |  Br+  |
+ * |-------------------------------------------------------------------------------------------------------|
+ * |       | Ctrl  |  Opt  |  Cmd  |       |  Del  | Space |       |       |       |       |       |  Br-  |
+ * `-------------------------------------------------------------------------------------------------------'
+ */
+    [_RAISE] = LAYOUT_ortho_5x13(
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_TAB,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_LCTL, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,   KC_PENT,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BRMU,
+        KC_NO,   KC_LCTL, KC_LOPT, KC_LCMD, KC_NO,   KC_DEL,  KC_SPC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BRMD
     )
 };
 
